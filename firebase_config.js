d206c230436732d8ba1c741bf8b8101f9b3d23e0
luzviva <okomarhlenka@gmail.com>
2025-07-03 12:38:39 -0300

    Update firebase_config.js

--- firebase_config.js
+++ firebase_config.js
@@ -1,17 +1,17 @@
-// firebase_config.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYTgt_CgQUp6kYfPeqc7JCaJuaeuEzP4Q",
+  authDomain: "multirotinaespacial.firebaseapp.com",
+  projectId: "multirotinaespacial",
+  storageBucket: "multirotinaespacial.firebasestorage.app",
+  messagingSenderId: "69790293324",
+  appId: "1:69790293324:web:3a2eb68b8beaeabb9ac3e9"
+};
 
-// O Netlify injetará o objeto window.firebaseConfig antes deste script ser executado.
-// Se o objeto não existir (ex: rodando localmente sem um setup), usamos um objeto vazio
-// para evitar erros, embora a conexão com o Firebase falhe.
-const firebaseConfig = window.firebaseConfig || {};
-
-// Inicializa o Firebase com a configuração injetada
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
